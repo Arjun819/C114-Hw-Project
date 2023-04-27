21a012010421c2081146040ec4aca263d622f58a
Arjun819 <87860512+Arjun819@users.noreply.github.com>
2023-04-27 12:00:35 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+if(b>a):
+    print("b is greater than a")
+elif(a == b):
+    print("a is equal to b")
+else:
+    print("a is greater than b")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + " Refnees")
+
+my_function("Email")
+my_function("Tofu")
+my_function("Linux")
